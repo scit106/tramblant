3e1312fef0323fb9014e7ad7f60b28e8ac3f7965
scit106 <scit106@gmail.com>
2013-07-26 21:49:33 -0400

    making repo

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Tramblant::Application.config.secret_token = 'c0e4f721e2505a9d39c0309460e4e92d4084fd0de5e1734eb9a820f8b7472347efaa9098378c999c389923735877c3edef1883689c6cc0bbbbe5a0524c479b73'
